d0a36e57b8715c2cee46a5e9b2170b10a2ef1e7e
Amithavinod <amithavinod2005@gmail.com>
2025-10-28 11:50:27 +0530

    completed

---
+++ record14b/cuboid.py
@@ -0,0 +1,4 @@
+def cuarea(l,b,h):
+    print("Area of cuboid",2*(l*b)+(l*h)+(b*h))
+def cuperi(l,b,h):
+    print("Perimeter of cuboid",4*(l+b+h))
---
+++ record14b/recta.py
@@ -0,0 +1,4 @@
+def rectarea(l,b):
+    print("Area of the rectangle",l*b)
+def recperi(l,b):
+    print("perimeter of the rectangle",2*(l+b))
