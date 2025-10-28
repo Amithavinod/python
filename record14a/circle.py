9c94e9ad65ed7dab271c4382aa8718faec3f6779
Amithavinod <amithavinod2005@gmail.com>
2025-10-28 11:49:44 +0530

    completed

---
+++ record14a/circle.py
@@ -0,0 +1,4 @@
+def cirarea(r):
+    print("Area of circle",3.14*r*r)
+def cirperi(r):
+    print("primeter of circle",2*3.14*r)
---
+++ record14a/rect.py
@@ -0,0 +1,4 @@
+def rectarea(l,b):
+    print("Area of rectangle:",l*b)
+def recperi(l,b):
+    print("perimeter of circle",2*(l*b))
